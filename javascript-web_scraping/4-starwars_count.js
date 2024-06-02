8212f45eed7bfe59b052405da6a32bae69882cf8
amenallahg122 <bigpapiamen@gmail.com>
2024-06-03 00:07:42 +0200

    js web scraping

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,13 @@
 #!/usr/bin/node
-const reqUrl = process.argv[2];
-const req = require('request');
 
-req.get(reqUrl, (err, res, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const movies = JSON.parse(body);
-    const WedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
-    const chars = movies.results[0].characters;
-    if (chars.includes(WedgeAntilles)) {
-      req.get(WedgeAntilles, (erro, resp, Body) => {
-        if (erro) {
-          console.error(erro);
-        } else {
-          const wedge = JSON.parse(Body);
-          const filmsCount = wedge.films.length;
-          console.log(filmsCount);
-        }
-      });
-    }
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    const moviesWithWedge = results.filter(movie =>
+      movie.characters.some(character => character.endsWith('/18/'))
+    );
+    console.log(moviesWithWedge.length);
   }
 });
